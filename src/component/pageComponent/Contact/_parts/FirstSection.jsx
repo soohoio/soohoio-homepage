92c851f708f3b235ef750b48602632217481a336
jihyuk <user.name>
2023-02-19 23:39:01 +0900

    Contact(without img layout) v00 Test(module.css)

--- src/component/pageComponent/Contact/_parts/FirstSection.jsx
+++ src/component/pageComponent/Contact/_parts/FirstSection.jsx
@@ -1,4 +1,5 @@
 import { BodyContainer } from '@/component/ui/BodyContainer';
+import styles from '../../../../pages/FontSelection.module.css';
 import { Box, Typography } from '@mui/material';
 import { useContext } from 'react';
 import DeviceContext from '@/module/ContextAPI/DeviceContext';
@@ -8,7 +9,7 @@ export default function FirstSection() {
   return (
     <BodyContainer ptPc="180px" pbPc="180px" ptMob="46px" pbMob="58px">
       <Typography
-        className={isMob ? 'mobTitle19KR' : 'pcTitle56KR'}
+        className={isMob ? styles.mobTitle19KR : styles.pcTitle56KR}
         component="div"
         color="primary"
         sx={{ fontWeight: 300, mb: { xs: '4px', sm: '21px' } }}
@@ -17,7 +18,10 @@ export default function FirstSection() {
         <Box sx={{ display: 'inline-block' }}>연결하고 수호합니다.</Box>
       </Typography>
 
-      <Typography className={isMob ? 'mobTitle14KR' : 'pcTitle36KR'} sx={{ fontWeight: 300 }}>
+      <Typography
+        className={isMob ? styles.mobTitle14KR : styles.pcTitle36KR}
+        sx={{ fontWeight: 300 }}
+      >
         수호와 함께 DeFi 생태계를 확장하세요.
       </Typography>
     </BodyContainer>
