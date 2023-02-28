e06ca609d901f3b8757e7d2916c54093f2d74913
jihyuk <user.name>
2023-03-01 01:18:10 +0900

    20230228_ver2.1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/contact': { page: '/contact' },
+      '/news': { page: '/news' },
+      '/product': { page: '/product' },
+      '/solution': { page: '/solution' },
+      '/solution/audit': { page: '/solution/audit' },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
