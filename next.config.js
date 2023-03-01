68b0738328744590a22c2b98dc6ee683f7d8201c
jihyuk <user.name>
2023-03-02 00:13:38 +0900

    20230228_ver2.1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       '/': { page: '/' },
